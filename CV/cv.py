de0e4ae7245c6f7a68919ecdaf0ef5f554a538b9
kovalekozlata <126063156+kovalekozlata@users.noreply.github.com>
2023-02-27 21:12:08 +0300

    Распознала лица по координатам, подключила вебку, нарисовала прямоугольнички вокруг лиц
    Обратилась к видеокамере, чтобы получить картинку с нее.
    Создала цикл для постоянного считывания картинки. Результат выполнения будет записан в переменную success, в img записывается картинка, которая считывается с экрана.
     В переменную "faces" закладываю список найденных координат наших лиц на картинке.
    Обращаюсь к найденным координатам лиц и вывожу зеленый прямоугольник вокруг них.
    Вывод результата на экран.

--- CV/cv.py
+++ CV/cv.py
@@ -3,15 +3,32 @@ import cv2
 # на 4 строке создаю переменную, указываю местоположение обученного интелекта в библиотеке open cv
 face_cascade_db = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml') 
 
-#пробую распознать лицо на картинке
-img = cv2.imread(r"C:\Users\User\Documents\GitHub\SmartHouse-SchoolX\CV\WIN_20230225_16_17_22_Pro.jpg")
-#преобразую картинку в оттенки серого
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#обращаюсь к видеокамере, чтобы получить картинку с нее
+cap = cv2.VideoCapture(0)
 
-faces = face_cascade_db.detectMultiScale(img_gray, 1.1, 19)
+#цикл для постоянного считывания картинки. Результат выполнения будет записан в переменную success, в img записывается картинка, которая считывается с экрана
+while True:
+    success, img = cap.read()
 
-#вывожу результат на экран
-cv2.imshow('rez',img_gray)
+    #пробую распознать лицо на картинке
+    #img = cv2.imread(r"C:\Users\User\Documents\GitHub\SmartHouse-SchoolX\CV\WIN_20230225_16_17_22_Pro.jpg")
+    #преобразую картинку в оттенки серого
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-#добавляю задержку
-cv2.waitKey()
+    #в переменную закладываю список найденных координат наших лиц на картинке
+    faces = face_cascade_db.detectMultiScale(img_gray, 1.1, 19)
+    #обращаюсь к найденным координатам лиц и вывожу зеленый прямоугольник вокруг них
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w, y+h),(0,255,0), 2)
+
+    #вывожу результат на экран
+    cv2.imshow('rez',img)
+
+    #добавляю задержку
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+
+#освобождаю камеру
+cap.release()
+cv2.destroyAllWindows()
+
